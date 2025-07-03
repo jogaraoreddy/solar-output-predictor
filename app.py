d39b5793742454589457ed9d478de8a8191a0360
jogaraoreddy <103505789+jogaraoreddy@users.noreply.github.com>
2025-07-03 22:45:32 +0530

    uploaded app.py folderlkk

---
+++ app.py
@@ -0,0 +1,6 @@
+import streamlit as st
+
+st.set_page_config(page_title="Solar Output Predictor")
+st.title("☀️ Solar Output Predictor (Coming Soon)")
+st.write("This app will predict solar energy generation using weather data and machine learning.")
+st.warning("🚧 Project is currently under development. Stay tuned!")
